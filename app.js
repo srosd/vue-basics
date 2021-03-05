6cd6132dc70edbfe9e493676aae34825b71a46e9
Sergio <srosduran@gmail.com>
2021-03-05 08:02:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoalA: 'Finish the course',
+            courseGoalB: 'Master the course',
+            vueLink: 'https://vuejs.org/'
+        };
+    },
+    methods: {
+        outputGoal(){
+            const randomNumber =  Math.random();
+            if (randomNumber < 0.5) return this.courseGoalA;
+            else return this.courseGoalB;
+        }
+    }
+});
+
+app.mount('#user-goal');
